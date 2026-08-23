4d4d48b0bd7ad56f6422b8699b9cb4a84e235e97
ethanng538 <ethanng538@proton.me>
2026-08-23 12:10:27 +0100

    fix: fix init.sql query

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,6 @@
 CREATE TABLE IF NOT EXISTS users (
      id SERIAL PRIMARY KEY,
      username VARCHAR(50) UNIQUE NOT NULL,
-    -- Allocates storage capacity to accommodate raw inputs or cryptographic hashes.
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
