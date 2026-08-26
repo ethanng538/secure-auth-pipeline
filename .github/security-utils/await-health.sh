fd05dfd0da58dbdf6d1ef1082f97fc9e9293b7ee
ethanng538 <ethanng538@proton.me>
2026-08-26 19:12:39 +0100

    ci: target port 3443 in health script

--- .github/security-utils/await-health.sh
+++ .github/security-utils/await-health.sh
@@ -1,6 +1,6 @@
 echo "Waiting for application to report healthy state..."
 for i in {1..30}; do
-  if curl -skL https://localhost/health | grep -q "healthy"; then
+  if curl -skL https://localhost:3443/health | grep -q "healthy"; then
     echo "✅ Secured edge architecture is online and reporting a healthy state."
     exit 0
   fi
